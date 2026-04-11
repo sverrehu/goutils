ffb63b02b4b00f8543e989a07fa377764d48e5f7
Sverre H Huseby <shh@thathost.com>
2026-04-11 21:48:25 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# goutils
+
+Makes life easier.
---
+++ lrumap/lrumap.go
@@ -0,0 +1,81 @@
+package lrumap
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	value        interface{}
+	whenLastUsed time.Time
+	whenExpires  time.Time
+}
+
+type LRUMap struct {
+	entries map[string]*cacheEntry
+	maxSize int
+	ttl     time.Duration
+	mutex   sync.Mutex
+}
+
+func New(maxSize int, ttl time.Duration) (m *LRUMap) {
+	return &LRUMap{entries: make(map[string]*cacheEntry, maxSize), maxSize: maxSize, ttl: ttl}
+}
+
+func (m *LRUMap) Put(key string, value interface{}) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	m.makeRoomForOne()
+	v, ok := m.entries[key]
+	if !ok {
+		v = &cacheEntry{}
+	}
+	now := time.Now()
+	v.value = value
+	v.whenLastUsed = now
+	v.whenExpires = now.Add(m.ttl)
+	m.entries[key] = v
+}
+
+func (m *LRUMap) Get(key string) interface{} {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	if v, ok := m.entries[key]; ok {
+		now := time.Now()
+		if now.After(v.whenExpires) {
+			delete(m.entries, key)
+			return nil
+		}
+		v.whenLastUsed = now
+		return v.value
+	}
+	return nil
+}
+
+func (m *LRUMap) Remove(key string) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	delete(m.entries, key)
+}
+
+func (m *LRUMap) makeRoomForOne() {
+	// map must be locked before calling
+	if len(m.entries) >= m.maxSize {
+		delete(m.entries, m.findLRUKey())
+	}
+}
+
+func (m *LRUMap) findLRUKey() string {
+	// map must be locked before calling
+	var lruKey string
+	var lruTime time.Time
+	first := true
+	for k, v := range m.entries {
+		if first || v.whenExpires.Before(lruTime) {
+			lruKey = k
+			lruTime = v.whenExpires
+			first = false
+		}
+	}
+	return lruKey
+}
